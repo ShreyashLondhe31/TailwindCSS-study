7d213421046e3d3a723a98077e1b47ac4d3c3046
ShreyashLondhe31 <shreyash.londhe@gmail.com>
2024-05-20 02:23:46 +0530

    Web agency project done

--- 2. Web Agency/tailwind.config.js
+++ 2. Web Agency/tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
-  content: ["./*.html"],
-  theme: {
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
-    },
-  },
-  plugins: [],
-};
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  // ...
+}
